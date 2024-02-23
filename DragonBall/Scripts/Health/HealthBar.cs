c280ffcd20d2a32335b3f0a288a5cfa5e405c108
tuantahe161117 <tuantahe161117@fpt.edu.vn>
2024-02-23 11:51:57 +0700

    tuan

---
+++ DragonBall/Scripts/Health/HealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image totalHealthBar;
+    [SerializeField] private Image currentHealthBar;
+
+    private void Start()
+    {
+        totalHealthBar.fillAmount = playerHealth.currentHealth / 10; // tai vi hinh anh thanh mau ban dau co 10 trai tim
+    }
+
+    private void Update()
+    {
+        currentHealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+}
---
+++ DragonBall/Scripts/Player/MapControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapControl : MonoBehaviour
+{
+    public List<GameObject> terrainChunks;
+    public GameObject player;
+    public float checkerRadius;
+    public LayerMask terrainMask;
+    private Vector3 noTerrainPosition;
+    public GameObject currentChunk;
+
+    int count = 0;
+    public int maxRender;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (count <= maxRender)
+        {
+            ChunkChecker();
+        }
+    }
+
+    void ChunkChecker()
+    {
+        if (!currentChunk)
+        {
+            return;
+        }
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (player != null) 
+        {
+            if (horizontal > 0)
+            {
+                
+                if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Right").position, checkerRadius, terrainMask))
+                {
+                    noTerrainPosition = currentChunk.transform.Find("Right").position;
+                    count++;
+                    SpawnChunk();
+                }
+            }
+        }
+
+    } 
+
+
+
+    void SpawnChunk()
+    {
+        if (terrainChunks.Count > 0)
+        {
+            int rand = Random.Range(0, terrainChunks.Count);
+            Instantiate(terrainChunks[rand], noTerrainPosition, Quaternion.identity);
+        }
+    }   
+}
